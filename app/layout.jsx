520df437307430fb788a71e1476bed51d4f7261a
Kwack <kwack@kwack.dev>
2025-03-11 20:32:59 +0800

    Fix sidebar links

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/LeoMavri/TSC-Documentation/tree/main"
           footer={footer}
           // ... Your additional layout options
         >
